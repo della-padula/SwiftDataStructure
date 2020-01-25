18a35d5e7a5778ff378ed142e831129c1ce2ed05
della-padula <della.kimko@gmail.com>
2020-01-25 15:54:25 +0900

    first commit

---
+++ SwiftDataStructure/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftDataStructure
+//
+//  Created by TaeinKim on 2020/01/25.
+//  Copyright © 2020 TaeinKim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
